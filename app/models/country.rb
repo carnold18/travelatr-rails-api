df85e6c434acae89c020e7b149f34cb94064d9f9
carnold18 <chrisarnold2010@gmail.com>
2019-12-08 21:50:29 -0800

    algorithm to scrape lodging days in country model complete

--- app/models/country.rb
+++ app/models/country.rb
@@ -4,24 +4,28 @@ class Country < ApplicationRecord
 
     @@country_id = 0
 
-    def total_expense
-        country = Country.find(@@country_id)
+    def total_expense(country)
         sum = 0
         country.expenses.each do |expense|
             sum += expense["value"]
         end
+        puts sum
         sum
     end
 
-    def scrape_total_days
-        byebug
+    def scrape_total_days(country)
+        days = country.expenses.where(category: "L").reduce(0) { |sum, expense| sum + expense[:name][-2].to_i }
+        puts days
+        days
     end
 
     def average_expense(id)
         @@country_id = id
-        @country = Country.find(id)
-        total = @country.total_expense
-        days = @country.scrape_total_days
+        country = Country.find(id)
+        total_cost = total_expense(country).to_f
+        total_days = scrape_total_days(country).to_f
+        avg = total_cost/total_days
+        avg
     end
 
 
